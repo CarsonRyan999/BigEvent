b3be571812e498b5d888ef68d2b7299dcdb504c4
maxiansheng <2818908400@qq.com>
2020-11-19 10:11:46 +0800

    发布项目

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -2,7 +2,8 @@
 // options: 当前AJAX请求的所有参数选项。
 $.ajaxPrefilter(function (options) {
     // 1、将根目录和接口地址拼接
-    options.url = "http://ajax.frontend.itheima.net" + options.url;
+    // options.url = "http://ajax.frontend.itheima.net" + options.url;
+    options.url = "http://120.27.211.140" + options.url;
 
     // 2、为有权限的接口设置headers请求头
     // 判断请求的地址中 是否包含/my/  如果包含 则需要加headers
@@ -14,7 +15,7 @@ $.ajaxPrefilter(function (options) {
 
     // 3、防止用户修改index.html来访问有权限的页面
     // 不论请求是否成功 jquery都会调用complete回调函数
-    options.complete = function (res) {``
+    options.complete = function (res) {
         // 可以通过res.responseJSON拿到服务器返回的数据
         if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
             // (1) 强制清空token
